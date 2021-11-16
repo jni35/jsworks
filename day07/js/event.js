7b1580baa3b81dfe09e0e4ba60253d2adc7a2c9b
jni3355 <wlsgml3835@gmil.com>
2021-11-16 12:50:29 +0900

    day07 추가

---
+++ day07/js/event.js
@@ -0,0 +1,17 @@
+//상세 설명 보기
+function showText(){
+    //설명 보이기
+    document.getElementById("desc").style.display = "block";
+
+    document.getElementById("open").style.display ="none";
+}
+
+//상세 설명 닫기
+function hidenText(){
+    //설명 숨기기
+    document.getElementById("desc").style.display = "none";
+
+    document.getElementById("open").style.display ="block";
+
+}
+
